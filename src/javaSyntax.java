1485981a723fe89cb968c47c8708fb05d77fb0e8
Art <art_jr2000@yahoo.com>
2017-09-18 15:22:11 -0500

    java syntax exercise

---
+++ src/javaSyntax.java
@@ -0,0 +1,83 @@
+public class javaSyntax {
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+
+        int myFavoriteNumber = 24;
+            System.out.println(myFavoriteNumber);
+
+
+//            Question 2- create a string
+//        String myString = "this is a string";
+//            System.out.println(myString);
+
+//        Question 3- received error because string needs double quotes. changed to char
+//        char myString = 'a';
+//        System.out.println(myString);
+
+//            Question 4-
+//        String myString = 3.14159;
+//        System.out.println(myString);
+
+//        Needed a double to accept decimal numbers
+//        double myString = 3.14159;
+//        System.out.println(myString);
+
+
+//         Question 5- error, expression needed
+//        long myNumber;
+//            System.out.println(myNumber);
+
+//        Question 6
+//        double myNumber = 3.14;
+//        int myNextNumber = (int) myNumber;
+//        System.out.println(myNumber);
+
+//        Question 7- the L does not show on the console
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+//        Question 8-
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+//        Question 9- error, float needs (f) at end of numbers
+//        float myNumber = 3.14;
+//        float myNumber = 3.14f;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x2 = 5;
+//        System.out.println(++x2);
+//        System.out.println(x2);
+//
+////        Had to rename the class
+//        String class2 = "class";
+//        System.out.println(class2);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//
+////        int x = 3;
+//        int y = 4;
+//        y = x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+
+
+
+
+
+    }
+}
