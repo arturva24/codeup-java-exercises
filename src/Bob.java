d1e98426de91769a2cfda2efc412617fa018f09a
Art <art_jr2000@yahoo.com>
2017-09-21 10:35:14 -0500

    StringExercise exercise

---
+++ src/Bob.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        while(true) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ask anything");
+        String msg = input.nextLine();
+
+
+            if (msg.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (msg.endsWith("!") || msg.equals(msg.toUpperCase()) && !msg.isEmpty()) {
+                System.out.println("Whoa, chill out!");
+            } else if (msg.isEmpty()) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever");
+            }
+            System.out.println("continue? y,n");
+            String option = input.nextLine();
+            if (!option.equalsIgnoreCase("y")){
+                break;
+            }
+        }
+
+
+    }
+
+}
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,55 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
-        long x = 2;
+//            Fizz Buzz exercise
 
-        do {
-            System.out.println(x);
-            x *= x;
-            }while(x <= 1000000);
+//        for (int counter = 1; counter <= 100; counter++) {
+//            if (counter % 3 == 0 && counter % 5 == 0) {
+//                System.out.println("FizzBuzz");
+////
+//            } else if (counter % 5 == 0) {
+//                System.out.println("Buzz");
+//
+//            } else if (counter % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(counter);
+//            }
 
+
+        while (true) {
+            System.out.println("Type in a number");
+            int number = input.nextInt();
+            input.nextLine();
+
+            System.out.println("number | squared | cubed\n" + "------ | ------- | ------");
+
+            for (int j = 1; j <= number; j++) {
+                System.out.printf("%-7s| ", j);
+                System.out.printf("%-8s| ", (j * j));
+                System.out.printf("%-5s", (j * j * j));
+                System.out.println();
+            }
+
+            System.out.println("Do you want to continue? y/n");
+            String option = input.nextLine();
+
+            if (!option.equalsIgnoreCase( "y")){
+                System.out.println("Ok, good bye");
+                break;
+            }
 
         }
-
     }
+}
+
+
+
+
+
+
 
