0d348c046cf7ce7acdb5412925835163eec4879a
Art <art_jr2000@yahoo.com>
2017-09-27 15:32:18 -0500

    changed some variables and wording

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -7,12 +7,12 @@ public class ServerNameGenerator {
         System.out.printf("Here is your server name:\n%s\n", serverName);
     }
 
-    public static final String[] adjective=
+    public static String[] adjective=
             {"Bitter", "Bland", "Delicious", "Fruity", "Minty", "Spicy", "Sweet", "Tangy", "Tasty", "Yummy"};
 
 
-    public static final String[] noun =
-            {"man","woman","cat","dog","bird", "mountain","ocean","airplane","tv","house"};
+    public static String[] noun =
+            {"Man","Woman","Cat","Dog","Bird", "Mountain","Ocean","Airplane","TV","House"};
 
 //        System.out.println(adjective[3]);
 //        System.out.println(noun[5]);
@@ -21,5 +21,14 @@ public class ServerNameGenerator {
     public static String randomWord(String[] words) {
         Random palabra = new Random();
         return words[palabra.nextInt(words.length)];
+
+//
+//     // alternate version
+//      public static String randomWord() {
+//           int rnd =(int) (Math.random() * ((noun.length ) + 1));
+//        int rnd2 =(int) (Math.random() * ((noun.length ) + 1));
+//
+//           return noun[rnd] + "-" + adjective[rnd2];
+//       }
     }
 }
